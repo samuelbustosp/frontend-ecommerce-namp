73bf313fdac5654ec4ae9b021486a3d16ef83c85
gonzabaldassi <gonzalobaldassi02@gmail.com>
2025-04-02 13:41:23 -0300

    Fix: Login y Register eliminacion de props innecesarios en el login y register  Cambios a ser confirmados: 	modificados:     src/components/admin/auth/Login.js 	modificados:     src/components/admin/auth/Register.js

--- src/components/admin/auth/Login.js
+++ src/components/admin/auth/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useUser } from '../../../contexts/UserContext';
 
-const Login = ({ setToken }) => {
+const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
--- src/components/admin/auth/Register.js
+++ src/components/admin/auth/Register.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Register = ({})=> {
+const Register = ()=> {
     const [name, setName] = useState('');
     const [lastname, setLastname] = useState('');
     const [email, setEmail] = useState('');
